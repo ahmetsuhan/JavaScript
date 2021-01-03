6d3f4691a9361eb46c817d1b95909f7c03c80c47
Ahmet Suhan OKA <58958906+ahmetsuhan@users.noreply.github.com>
2021-01-03 22:45:55 +0300

    Add files via upload

---
+++ 9-OOP/15-APP_Call_Apply_Bind/scripts.js
@@ -0,0 +1,29 @@
+// Demo : Numeric Range
+
+
+var num = {
+    min: 0,
+    max: 100,
+    checkNumericRange: function(value) {
+        if (typeof(value) !== "number") {
+            return false;
+        } else {
+            return value >= this.min && value <= this.max;
+        }
+    }
+}
+console.log(num.checkNumericRange(20));
+console.log(num.checkNumericRange(-50));
+console.log(num.checkNumericRange(101));
+console.log(num.checkNumericRange("50"));
+console.log("*****************");
+
+var num1 = {
+    min: 10,
+    max: 30
+};
+console.log(num.checkNumericRange.call(num1, 55));
+console.log(num.checkNumericRange.apply(num1, [55]));
+
+var checkNumber = num.checkNumericRange.bind(num1);
+console.log(checkNumber(59));
